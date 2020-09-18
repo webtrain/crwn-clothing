4522f1e4548627ffb040ad410d7451dd969cde92
webtrain <32815688+laszloh111@users.noreply.github.com>
2020-09-18 12:29:39 +0200

    update our menu-items to navigate withRouter higher order component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -41,19 +41,14 @@ class Directory extends React.Component {
           linkUrl: 'shop/mens',
         },
       ],
-    
-    
-    
-    
-    
     };
   }
 
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
